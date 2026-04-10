8dd353f8d355a0522d3c633cbceea1c6737807a8
hefe00935 <halukefenasa@outlook.com>
2026-04-11 01:44:24 +0300

    LFS

---
+++ main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9f3e4c8f4899060715991ac3d1b610b57325813b37ef84d5f0426ef82ba722bf
+size 58655
---
+++ updater.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ed9e7e686dd5624c5857935f47903dc0cb003753cf80482998475d2745aaa172
+size 2364
